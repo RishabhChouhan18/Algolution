6eaa3b5416531e20bb69a14444a5261890912eef
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2024-10-18 08:09:09 +0530

    Update Parellelogram.java

--- Parellelogram.java
+++ Parellelogram.java
@@ -1,19 +1,19 @@
 public class Parellelogram {
     public static void main(String[] args) {
-        int rows = 5; // Parallelogram ki rows ki sankhya
-        int columns = 10; // Parallelogram ki columns ki sankhya
+        int rows = 5; 
+        int columns = 10; 
 
-        // Outer loop rows ke liye
+       
         for (int i = 0; i < rows; i++) {
-            // Spaces print karna
+           
             for (int j = 0; j < i; j++) {
-                System.out.print(" "); // Space print karte hain
+                System.out.print(" "); 
             }
-            // Star print karna
+           
             for (int j = 0; j < columns; j++) {
-                System.out.print("*"); // Star print karte hain
+                System.out.print("*"); 
             }
-            // New line ke liye
+            
             System.out.println();
         }
     }
