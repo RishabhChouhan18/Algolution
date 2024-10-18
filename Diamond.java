8240911902081178a9124604c8980ba3b0e86d75
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2024-10-18 08:00:27 +0530

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,32 +1,32 @@
 public class Diamond {
     public static void main(String[] args) {
-        int n = 5; // Half diamond ki height
+        int n = 5; 
 
-        // Upper half diamond
+        
         for (int i = 1; i <= n; i++) {
-            // Spaces print karna
+            
             for (int j = n; j > i; j--) {
-                System.out.print(" "); // Space print karte hain
+                System.out.print(" "); 
             }
             // Stars print karna
             for (int j = 1; j <= i; j++) {
-                System.out.print("* "); // Star print karte hain
+                System.out.print("* ");
             }
-            // New line ke liye
+            
             System.out.println();
         }
 
-        // Lower half diamond
+        
         for (int i = n - 1; i >= 1; i--) {
-            // Spaces print karna
+            
             for (int j = n; j > i; j--) {
-                System.out.print(" "); // Space print karte hain
+                System.out.print(" "); 
             }
-            // Stars print karna
+            
             for (int j = 1; j <= i; j++) {
-                System.out.print("* "); // Star print karte hain
+                System.out.print("* "); 
             }
-            // New line ke liye
+            
             System.out.println();
         }
     }
