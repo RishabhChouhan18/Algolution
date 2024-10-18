5f7fee5729373ca77fbf65c85ef60df6e1c6e0d8
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2024-10-18 08:03:35 +0530

    Update KthLargestElement.java

--- KthLargestElement.java
+++ KthLargestElement.java
@@ -9,19 +9,16 @@ public class KthLargestElement {
     }
 
     public static int findKthLargest(int[] nums, int k) {
-        // Min-heap banate hain
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        // Array ke elements ko heap me daalte hain
         for (int num : nums) {
             minHeap.add(num);
-            // Agar heap ki size k se zyada ho jaye to chhote element ko remove karte hain
             if (minHeap.size() > k) {
                 minHeap.poll();
             }
         }
 
-        // Heap ka top element kth largest hoga
+       
         return minHeap.peek();
     }
 }
