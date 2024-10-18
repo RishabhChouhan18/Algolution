3bcae0ee79551e668f4972569c6819c671f2351d
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2024-10-18 08:10:03 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,17 +1,17 @@
 public class Pattern2 {
     public static void main(String[] args) {
-        int n = 5; // Size of the pattern
+        int n = 5; 
 
-        // Loop for printing the pattern
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("1"); // Print 1 for the borders
+                    System.out.print("1");
                 } else {
-                    System.out.print("0"); // Print 0 for the inside
+                    System.out.print("0"); 
                 }
             }
-            System.out.println(); // New line ke liye
+            System.out.println(); 
         }
     }
 }
