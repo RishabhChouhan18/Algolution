28e7e703f8a80098a6928d157a04ea3435d63f4f
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2024-12-01 22:33:06 +0530

    Update KthLargestElement.java

--- KthLargestElement.java
+++ KthLargestElement.java
@@ -22,3 +22,19 @@ public class KthLargestElement {
         return minHeap.peek();
     }
 }
+/*
+import java.util.Arrays;
+
+public class KthLargestElement {
+    public static int findKthLargest(int[] arr, int k) { 
+        Arrays.sort(arr);
+        return arr[arr.length - k];
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 8, 7, 2};
+        int k = 3;
+        int result = findKthLargest(array, k);
+        System.out.println("The kth largest element is: " +result);
+    }
+}*/
