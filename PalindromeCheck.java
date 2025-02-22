f832ae911a6ea5ac5a17aa8a937d2b9e8950011a
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2025-02-22 22:58:36 +0530

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,21 +1,27 @@
-class PalindromeCheck{
-    static boolean check(int[] array){
-       int l= 0;
-       int r= array.length-1;
-        
-        while(l<=r){
-            if(array[l]!= array[r]){
-                 System.out.println("array is not palindrome");
-                 return false;
+import java.util.Scanner;
+class CheckPalindrome {
+        static boolean check(int[] array){
+            int l= 0;
+            int r= array.length-1;
+
+            while(l<=r){
+                if(array[l]!= array[r]){
+                    System.out.println("array is not palindrome");
+                    return false;
+                }
+                l++;
+                r--;
             }
-             l++;
-             r--;
+            System.out.println("array is palindrome");
+            return true;
         }
-        System.out.println("array is palindrome");
-        return true;
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int[] array  = new int[5];
+            for(int i =0 ;i<array.length ; i++){
+                array[i] = sc.nextInt();
+            }
+            check(array);
+        }
     }
-    public static void main(String[] args){
-        int[] array = {44,56,56,44};
-        check(array);
-    }
-}
+
