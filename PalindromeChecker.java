70f6199aa953fb1183bfe5cf2f171d3f7f226a27
RishabhChouhan18 <163510810+RishabhChouhan18@users.noreply.github.com>
2025-02-22 22:57:58 +0530

    Update PalindromeChecker.java

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,30 +1,4 @@
-//public class PalindromeChecker {
-//    public static void main(String[] args) {
-//        String s1 = "A man, a plan, a canal: Panama";
-//        String s2 = "race a car";
-//
-//        System.out.println("Is the first string a palindrome? " + isPalindrome(s1));
-//        System.out.println("Is the second string a palindrome? " + isPalindrome(s2));
-//    }
-//
-//    public static boolean isPalindrome(String s) {
-//        StringBuilder cleanedString = new StringBuilder();
-//
-//        // String ko process karte hain
-//        for (char c : s.toCharArray()) {
-//            // Agar character alphanumeric hai to usse lowercase me convert karke cleanedString me daalte hain
-//            if (Character.isLetterOrDigit(c)) {
-//                cleanedString.append(Character.toLowerCase(c));
-//            }
-//        }
-//
-//        // Cleaned string ko reverse karte hain
-//        String reversedString = cleanedString.reverse().toString();
-//
-//        // Check karte hain agar cleaned string original cleaned string ke barabar hai
-//        return cleanedString.toString().equals(reversedString);
-//    }
-//}
+
 public class PalindromeChecker {
     public static boolean isPalindrome(String s) {
         String cleaned = "";
